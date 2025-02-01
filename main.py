57e6b6b07b4c7be94a975105454c9f97d648a34a
Vinay Chabarwal <elvincentonino@gmail.com>
2025-02-01 12:32:22 +0530

    Updated database, models, and added authentication-related files

--- main.py
+++ main.py
@@ -1,11 +1,58 @@
 from fastapi import FastAPI
+from typing import Optional
+from pydantic import BaseModel
+
+
 
 app = FastAPI()
 
+@app.get('/blog')
+def index(limit=20, published: bool = True, sort: Optional[str] = None):
+    #only get 10 published blogs
+    if published:
+        return {'data':f'{limit} published blogs from the db'}
+    else:
+        return {'data':f'{limit} blogs from the db'}
+
+
+
+
+
+
+
+
+
+
 @app.get('/')
 def index():
-    return {'data': {'name': 'Sarthak'}}
+    return {'data': 'blog list'}
 
-@app.get('/about')
-def about():
-    return {'data':{'about page'}}
+@app.get('/blog/unpublished')
+def unpublished():
+    return {'data':'all unpublished blogs'}
+
+@app.get('/blog/{id}')
+def show(id: int):
+    return {'data':id}
+
+
+
+@app.get('/blog/{id}/comments')
+def comments(id, limit=10):
+    # fetch comments of blog with id = id
+    # return limit
+    return {'data':{'1', '2'}}
+
+
+
+class Blog(BaseModel):
+    title: str
+    body: str
+    published: Optional[bool] = False
+
+
+
+@app.post('/blog')
+def create_blog(blog: Blog):
+    return {'data': f"Blog is created with title as {blog.title}"}
+    # return {'data': "Blog is created"}
